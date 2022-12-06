04874f4d9e6779594dfaf14e9b196efb68b9c807
Tri Tue Mai <HacTinhThan@gmail.com>
2022-12-06 15:59:39 +0700

    'railway02'

--- teddybearbackend/settings.py
+++ teddybearbackend/settings.py
@@ -112,6 +112,13 @@ else:
         }
     }
 
+# .env
+# DATABASE_NAME=postgres
+# DATABASE_USER=postgres
+# DATABASE_PASS=WAWdH3d7@kx2Kk2
+# DATABASE_PORT=5432
+# DATABASE_HOST=db.zzctorvlqfeehupirwoj.supabase.co
+# ENGINE=django.db.backends.postgresql_psycopg2
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -148,10 +155,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = '%s/static/' % BASE_DIR
 CKEDITOR_UPLOAD_PATH = 'blogPost/'
+# co tat tep static hay khoong  tat = 1
+DISABLE_COLLECTSTATIC = 0
 # CKEDITOR_BASEPATH = "/blogPost/ckeditor/ckeditor/"
 # CKEDITOR_CONFIGS = {
 #     'awesome_ckeditor': {
